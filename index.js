8686d6678c8dfa64811adabc18b934714b521224
dacsancomchayninhbinh1-code <dacsancomchayninhbinh1@gmail.com>
2026-04-19 21:05:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -60,3 +60,7 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
