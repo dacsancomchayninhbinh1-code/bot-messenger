97683c04da254aacf756c5b0e0ebec14742c5573
dacsancomchayninhbinh1-code <dacsancomchayninhbinh1@gmail.com>
2026-04-21 14:17:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-import express from "express";
-const app = express();
-app.use(express.json());
+const express = require("express");
 
-const VERIFY_TOKEN = "abc123";
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("OK");
 });
 
 app.get("/webhook", (req, res) => {
-  console.log("🔥 WEBHOOK HIT");
+  const VERIFY_TOKEN = "abc123";
 
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
-
-  console.log("MODE:", mode);
-  console.log("TOKEN:", token);
-  console.log("CHALLENGE:", challenge);
 
   if (mode === "subscribe" && token === VERIFY_TOKEN) {
     return res.send(challenge);
@@ -26,12 +20,8 @@ app.get("/webhook", (req, res) => {
   return res.sendStatus(403);
 });
 
-app.post("/webhook", (req, res) => {
-  res.sendStatus(200);
-});
-
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-  console.log("🚀 Server running on port " + PORT);
+  console.log("RUNNING");
 });
